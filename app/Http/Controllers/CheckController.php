a585b9b39e491442c9d81e9293a3aca3e6c964a6
OusmaneFane <ousmanefane08@gmail.com>
2023-12-20 21:23:06 +0000

    deleted dd

--- app/Http/Controllers/CheckController.php
+++ app/Http/Controllers/CheckController.php
@@ -23,7 +23,7 @@ class CheckController extends Controller
         // Vérifier si le temps de vote est dépassé
         $currentTime = now(); // Obtenez la date et l'heure actuelles
         $votingEndTime = Carbon::create($currentTime->year, 12, 21, 23, 59, 59);
-        dd($votingEndTime); // Définir la fin du temps de vote
+        // Définir la fin du temps de vote
         if ($currentTime > $votingEndTime) {
             // Temps de vote dépassé, affichez un message et redirigez l'utilisateur
             return back()->with(
