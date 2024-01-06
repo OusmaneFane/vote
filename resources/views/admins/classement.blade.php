23212df6fadf9189cbc4d55f41ae96e717f32b25
OusmaneFane <ousmanefane08@gmail.com>
2024-01-06 01:11:55 +0000

    somes bugs

--- resources/views/admins/classement.blade.php
+++ resources/views/admins/classement.blade.php
@@ -22,13 +22,13 @@
 <div class="col-12 grid-margin">
     <div class="card">
         <div class="card-body">
-            <h4 class="card-title">Classement de l'ELECTION LEADER MANAGER 2023-2024</h4>
+            <h4 class="card-title">Classement ELECTION LEADER MANAGER 2023-2024</h4>
             <p class="card-description"></p>
             <div class="row">
                 @foreach ($candidats as $candidat )  
                 <div class="col-md-4 h-100">
                     <div class="bg-{{ $candidat->color}} p-4 @if($candidat->titre === 'Président LEADER MANAGER') president-animation @endif fireworks-container">
-                        <h6 class="card-title">{{ $candidat->titre}}</h6>
+                        <h2 class="card-title">{{ $candidat->titre}}</h2>
                         <div id="profile-list-left" class="py-2">
                             <div class="card rounded mb-2">
                                 <div class="card-body p-3">
